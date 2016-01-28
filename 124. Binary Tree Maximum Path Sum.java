f6d8fcbb543eb8c7bada446fa500b9380dd01a7a
Jie Zhou <jeffjiezhou@gmail.com>
2016-01-28 12:02:47 -0500

    Binary Tree Maximum Path Sum

---
+++ 124. Binary Tree Maximum Path Sum.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int max;
+
+    public int maxPathSum(TreeNode root) {
+        max = Integer.MIN_VALUE;
+        helper(root);
+        return max;
+    }
+
+    public int helper(TreeNode root) {
+        if(root == null)
+            return 0;
+
+        int left = Math.max(0, helper(root.left));
+        int right = Math.max(0, helper(root.right));
+
+        max = Math.max(max, root.val + left + right);
+
+        return root.val + Math.max(left, right);
+    }
+}
