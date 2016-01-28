46657cc5257174b257f194570ac27c7be9765818
Jie Zhou <jeffjiezhou@gmail.com>
2016-01-28 17:13:50 -0500

    Search a 2D Matrix II

---
+++ 240. Search a 2D Matrix II.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int col = matrix[0].length-1;
+        int row = 0;
+
+        while(col >= 0 && row <= matrix.length-1) {
+            if(target == matrix[row][col]) {
+                return true;
+            } else if(target < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+}
