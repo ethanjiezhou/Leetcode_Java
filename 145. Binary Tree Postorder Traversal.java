c8e812695ecc42ef3c9dd353317658f1a6b5d077
Jie Zhou <jeffjiezhou@gmail.com>
2016-01-27 22:38:58 -0500

    Binary Tree Postorder Traversal

---
+++ 145. Binary Tree Postorder Traversal.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        helper(root, list);
+        return list;
+    }
+
+    public void helper(TreeNode root, List<Integer> list) {
+        if(root == null)
+            return;
+
+        helper(root.left, list);
+        helper(root.right, list);
+        list.add(root.val);
+    }
+}
