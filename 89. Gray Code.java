6fcd3f0dfd00923df99b5e7890b16cd1419df0ca
Jie Zhou <jeffjiezhou@gmail.com>
2016-01-29 01:08:10 -0500

    Gray Code

---
+++ 89. Gray Code.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < 1 << n; i++) list.add(i ^ i >> 1); // G(i) = i ^ (i / 2)
+        return list;
+    }
+}
