d0e7c435a0523b56d64c90e82ca0e8c06e3e9707
Jie Zhou <jeffjiezhou@gmail.com>
2016-01-30 22:00:54 -0500

    Contains Duplicate II

---
+++ 219. Contains Duplicate II.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums == null || nums.length == 0|| nums.length == 1 || k == 0)
+            return false;
+
+        k = k > nums.length ? nums.length : k;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i]))
+                return true;
+
+            set.add(nums[i]);
+
+            if(i >= k)
+                set.remove(nums[i-k]);
+        }
+        return false;
+    }
+}
