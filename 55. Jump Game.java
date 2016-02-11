c505c1ae440a73fd307fb2488200df93df048e8e
Jie Zhou <jeffjiezhou@gmail.com>
2016-02-11 16:34:24 -0500

    Jump Game

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -1,3 +1,28 @@
+//version 1
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int last = nums.length - 1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if(i + nums[i] >= last)
+                last = i;
+        }
+        return last <= 0;
+    }
+}
+
+//version 2
+public class Solution {
+    public boolean canJump(int[] A) {
+    int max = 0;
+    for(int i=0;i<A.length;i++){
+        if(i>max) {return false;}
+            max = Math.max(A[i]+i,max);
+    }
+        return true;
+    }
+}
+
+//version 3
 public class Solution {
     public boolean canJump(int[] nums) {
         int currentMaxIndex = 0;
