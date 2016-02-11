003960f4ea0e6b594873921ca1cf31623a18e31d
Jie Zhou <jeffjiezhou@gmail.com>
2016-02-11 12:27:33 -0500

    Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock.java
+++ 121. Best Time to Buy and Sell Stock.java
@@ -1,3 +1,4 @@
+// version 1
 public class Solution {
     public int maxProfit(int[] prices) {
         if(prices == null || prices.length == 0)
@@ -9,5 +10,18 @@ public class Solution {
             max = Math.max(max, prices[i] - min);
         }
         return max;
+    }
+}
+
+// version 2
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int maxCur = 0;
+        int maxSofar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i - 1]);
+            maxSofar = Math.max(maxSofar, maxCur);
+        }
+        return maxSofar;
     }
 }
