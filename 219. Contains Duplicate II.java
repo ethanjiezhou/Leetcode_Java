1ef71b1e7b0f126761487f61b5115a8a7614c66f
Jie Zhou <jeffjiezhou@gmail.com>
2016-02-11 22:50:42 -0500

    Contains Duplicate II

--- 219. Contains Duplicate II.java
+++ 219. Contains Duplicate II.java
@@ -17,3 +17,16 @@ public class Solution {
         return false;
     }
 }
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k)
+                set.remove(nums[i-k-1]);
+            if(!set.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
