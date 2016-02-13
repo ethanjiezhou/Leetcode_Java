b2d37e299c61ff7687d37289d9d74f7dd6c1f8d5
Jie Zhou <jeffjiezhou@gmail.com>
2016-02-13 18:00:34 -0500

    Subsets II

--- 90. Subsets II.java
+++ 90. Subsets II.java
@@ -1,25 +1,17 @@
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
+        list.add(new ArrayList<Integer>()); // add null list
         Arrays.sort(nums);
         for(int value : nums) {
-            if(list.isEmpty()) {
-                list.add(new ArrayList(Arrays.asList(value)));
-            } else {
-                int size = list.size();
-                for(int i = 0; i < size; i++) {
-                    List<Integer> temp = new ArrayList<>(list.get(i));
-                    temp.add(value);
-                    if(!list.contains(temp))
-                        list.add(temp);
-                }
-                List<Integer> check = new ArrayList(Arrays.asList(value));
-                if(!list.contains(check))
-                    list.add(check);
+            int size = list.size();
+            for(int i = 0; i < size; i++) {
+                List<Integer> temp = new ArrayList<>(list.get(i));
+                temp.add(value);
+                if(!list.contains(temp))
+                    list.add(temp);
             }
         }
-
-        list.add(new ArrayList<Integer>());
         return list;
     }
 }
