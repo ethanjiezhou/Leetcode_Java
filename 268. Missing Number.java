d99ce6547b2b94b030604322f957a20351bac7f2
Jie Zhou <jeffjiezhou@gmail.com>
2016-02-14 22:24:46 -0500

    Missing Number

---
+++ 268. Missing Number.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int index = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            index -= nums[i] - i;
+        }
+        return index;
+    }
+}
+
+// version 2
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = (0 + nums.length) * (nums.length + 1) / 2;
+        for(int value : nums) {
+            sum -= value;
+        }
+        return sum;
+    }
+}
+
+
+// version 3
+public class Solution {
+    public int missingNumber(int[] nums) {
+
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor ^ i;
+    }
+}
