ed07c3bb72642003ed9ac82f827b3d9c2f9244c4
Jie Zhou <jeffjiezhou@gmail.com>
2016-03-09 21:09:56 -0500

    Power of Three

---
+++ 326. Power of Three.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        return n > 0 && (1162261467 % n == 0);
+    }
+}
+
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n > 1)
+            while(n % 3 == 0)
+                n = n / 3;
+        return n == 1;
+    }
+}
+
+
+
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        return n > 0 && (n == 1 || (n % 3 == 0 && isPowerOfThree(n / 3)));
+    }
+}
