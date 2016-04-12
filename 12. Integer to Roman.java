2c6ee2887b3591a9459cd7c40905ddd048ae5dc8
Jie Zhou <jeffjiezhou@gmail.com>
2016-04-12 16:55:16 -0400

    Integer to Roman

--- 12. Integer to Roman.java
+++ 12. Integer to Roman.java
@@ -1,16 +1,46 @@
 public class Solution {
-    public String intToRoman(int num) {
-        if (num == 0)
-            return "N";
-        String[] strs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < values.length; i++) {
-            while(num >= values[i]) {
-                num -= values[i];
-                sb.append(strs[i]);
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(map.containsKey(a)) {
+                if(map.get(a).equals(b)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                if(!map.containsValue(b)) {
+                    map.put(a, b);
+                } else {
+                    return false;
+                }
             }
         }
-        return sb.toString();
+        return true;
     }
 }
+/*
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+        Map<Character, Character> left = new HashMap<Character, Character>();
+        Map<Character, Character> right = new HashMap<Character, Character>();
+        for(int i = 0; i < s.length(); i++) {
+            if(left.containsKey(s.charAt(i)) && left.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            } else if(right.containsKey(t.charAt(i)) && right.get(t.charAt(i)) != s.charAt(i)) {
+                return false;
+            } else {
+                left.put(s.charAt(i), t.charAt(i));
+                right.put(t.charAt(i), s.charAt(i));
+            }
+        }
+        return true;
+    }
+}
+*/
