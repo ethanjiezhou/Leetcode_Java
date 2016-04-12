3b2809d757f9c278bddc466f59e22234eeecdf79
Jie Zhou <jeffjiezhou@gmail.com>
2016-04-12 00:19:42 -0400

    Rectangle Area

--- 223. Rectangle Area.java
+++ 223. Rectangle Area.java
@@ -11,3 +11,13 @@ public class Solution {
         return (C - A)*(D - B) + (G - E)*(H - F) - overlap;
     }
 }
+
+/*
+public class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int left = Math.max(A, E), right = Math.max(Math.min(C, G), left);
+        int bottom = Math.max(B, F), top = Math.max(Math.min(D, H), bottom);
+        return (C - A) * (D - B) + (G - E) * (H - F) - (right - left) * (top - bottom);
+    }
+}
+ */
