96ecc227643a83ba0bec40d1b502fdb99b914c57
Jie Zhou <jeffjiezhou@gmail.com>
2016-04-12 22:25:10 -0400

    Valid Parentheses

---
+++ 20. Valid Parentheses.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if(ch == ')' && !stack.empty() && stack.peek() == '(') {
+                    stack.pop();
+                } else if(ch == '}' && !stack.empty() && stack.peek() == '{') {
+                    stack.pop();
+                } else if(ch == ']' && !stack.empty() && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
