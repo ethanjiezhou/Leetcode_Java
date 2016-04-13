2ee571f80b1a457d6fad82787173315f02604573
Jie Zhou <jeffjiezhou@gmail.com>
2016-04-12 20:37:51 -0400

    Integer to Roman

--- 12. Integer to Roman.java
+++ 12. Integer to Roman.java
@@ -1,4 +1,16 @@
 public class Solution {
+    public boolean isIsomorphic(String s1, String s2) {
+        int[] m = new int[512];
+        for (int i = 0; i < s1.length(); i++) {
+            if(m[s1.charAt(i)] != m[s2.charAt(i)+256])
+                return false;
+            m[s1.charAt(i)] = m[s2.charAt(i)+256] = i+1;
+        }
+        return true;
+    }
+}
+/*
+public class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length() != t.length())
             return false;
@@ -23,6 +35,7 @@ public class Solution {
         return true;
     }
 }
+*/
 /*
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
